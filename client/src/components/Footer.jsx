d7830a536b52ed60d8f02378bd2d11fc2292598e
Victor Pinto <victorpintowake@gmail.com>
2023-08-13 16:09:19 -0300

    final 01

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
           <div className="footer-text">
             <p>
               &copy; 2023 <span>Victor Pinto</span> | {t('reserved')}</p>
-            <p className="mail">victorpintowake@gmail.com</p>
+            <p className="mail">Email: victorpintowake@gmail.com - whatsapp: +54 1131158866 - Location: Argentina, Buenos Aires</p>
           </div>
           <div className="footer-iconTop">
             <a href="#home">
